e03e96b42e038a158ed57b7e26760c31b513f8b1
Henrique Vital Carvalho <48650626+henriquevital00@users.noreply.github.com>
2021-09-10 00:01:43 -0300

    Deleted wrong variable token

--- Tokens/Definitions/Variables/VarTypes/VarTypes.py
+++ Tokens/Definitions/Variables/VarTypes/VarTypes.py
@@ -4,7 +4,6 @@ from Tokens.TokenTypes.TokenTypes import TokenTypes
 
 class VariableTypesTokens(Enum):
 
-    VAR_NAME = Token('VAR_NAME', TokenTypes.VAR_TYPE)
     NUMBER = Token('NUMBER', TokenTypes.VAR_TYPE)
     STRING = Token('STRING', TokenTypes.VAR_TYPE)
     BOOLEAN = Token('BOOL', TokenTypes.VAR_TYPE)
