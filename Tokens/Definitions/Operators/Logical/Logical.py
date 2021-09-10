298c8b310dc2ec67e4ea5b40a761f06222c03b73
GabrielBueno200 <56837996+GabrielBueno200@users.noreply.github.com>
2021-09-10 00:45:08 -0300

    Fixed wrong name tokens

--- Tokens/Definitions/Operators/Logical/Logical.py
+++ Tokens/Definitions/Operators/Logical/Logical.py
@@ -5,5 +5,5 @@ from Tokens.TokenTypes.TokenTypes import TokenTypes
 
 class LogicalOperatorsTokens(Enum):
 
-    AND = Token('GREATER', TokenTypes.OPERATOR)
-    OR = Token('GREATER', TokenTypes.OPERATOR)
+    AND = Token('AND', TokenTypes.OPERATOR)
+    OR = Token('OR', TokenTypes.OPERATOR)
