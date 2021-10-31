1d76123ceb10bffcd2a1d75882f885e5fbb5650d
GabrielBueno200 <gabrielbueno200@gmail.com>
2021-10-31 19:07:42 -0300

    added var assign and declare

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -1,5 +1,4 @@
 from Lexer.Lexer import Lexer
-from Parser.SyntaxEvaluator.SyntaxEvaluator import SyntaxEvaluator
 from Parser.SyntaxMatcher.SyntaxMatcher import SyntaxMatcher
 from Parser.SyntaxTree import SyntaxTree
 from Parser.SyntaxTypes.Expression.ArithmeticExpression import ArithmeticExpression
@@ -9,8 +8,10 @@ from Parser.SyntaxTypes.Expression.LiteralExpression.NumberExpression import Num
 from Parser.SyntaxTypes.Expression.LiteralExpression.StringExpression import StringExpression
 from Parser.SyntaxTypes.Expression.LogicalExpression import LogicalExpression
 from Parser.SyntaxTypes.Expression.ParenthesizedExpression import ParenthesizedExpression
-from Parser.SyntaxTypes.Expression.BinaryExpression import BinaryExpression
 from Parser.SyntaxTypes.Expression.Expression import Expression
+from Parser.SyntaxTypes.Statement.SimpleStatement.SimpleStatement import SimpleStatement
+from Parser.SyntaxTypes.Statement.SimpleStatement.VarAssignSyntax import VarAssignSyntax
+from Parser.SyntaxTypes.Statement.SimpleStatement.VarDeclareSyntax import VarDeclareSyntax
 from Tokens.Constants.TokenConstants import TokenTypes
 from Tokens.Token import Token
 from core import core
@@ -109,7 +110,7 @@ class Parser:
     # LOGICAL_FACTOR -> IDENTIFIER
     #                   | LITERAL
     #                   | EXPR
-    #                   | L_PAREN LOGICAL_EXPRR_PAREN
+    #                   | L_PAREN LOGICAL_EXPR R_PAREN
 
 
     def parseLogicalTerm(self) -> Expression:
@@ -248,7 +249,7 @@ class Parser:
 
     # endregion
 
-    # region CONDITIONAL EXPRESSION
+    # region 4. CONDITIONAL EXPRESSION
 
     # CONDITIONAL_EXPR -> EXPR | LITERAL | IDENTIFIER
 
@@ -281,9 +282,77 @@ class Parser:
             else:
                 return NumberExpression(literalToken)
 
+    # endregion
+
+    # region 5. SIMPLE STATEMENT
+
+    # SIMPLE_STMT -> ASSIGN | DECLARE
+    #
+    # ASSIGN -> VAR_TYPE IDENTIFIER VAR_ASSIGN_OPERATOR (LITERAL | IDENTIFIER | EXPR) ENDCOMMAND
+    # 	         | IDENTIFIER VAR_ASSIGN_OPERATOR (LITERAL | IDENTIFIER | EXPR) ENDCOMMAND
+    #
+    # DECLARE -> VAR_TYPE IDENTIFIER ENDCOMMAND
+
+    def parseSimpleStatement(self) -> SimpleStatement:
+        var_type = None
+
+        if self.current_token.type in (
+                TokenTypes.TYPE_BOOLEAN,
+                TokenTypes.TYPE_STRING,
+                TokenTypes.TYPE_NUMBER
+        ):
+            var_type = self.current_token
+            self.eat(var_type.type)
+
+        if self.current_token.type == TokenTypes.IDENTIFIER:
+            identifier = self.current_token
+            self.eat(identifier.type)
+
+            if self.current_token.type == TokenTypes.END_COMMAND and var_type:
+                return VarDeclareSyntax(var_type, identifier)
+
+            if self.current_token.type == TokenTypes.VAR_ASSIGN:
+                operator = self.current_token
+                self.eat(operator.type)
+
+                value = None
+
+                #  IS EXPRESSION
+                expression = SyntaxMatcher.checkSyntax([
+                    [LogicalExpression, self.parseLogicalTerm],
+                    [ComparisonExpression, self.parseComparisonExpression],
+                    [ArithmeticExpression, self.parseArithmeticTerm],
+                ], self)
+
+                if expression:
+                    value = expression
+                elif self.current_token.type in (
+                        TokenTypes.NUMBER_LITERAL,
+                        TokenTypes.BOOLEAN_LITERAL,
+                        TokenTypes.STRING_LITERAL
+                ):
+                    literalToken = self.current_token
+                    self.eat(self.current_token.type)
+
+                    if literalToken.type == TokenTypes.STRING_LITERAL:
+                        value = StringExpression(literalToken)
+
+                    elif literalToken.type == TokenTypes.BOOLEAN_LITERAL:
+                        value = BooleanExpression(literalToken)
+
+                    else:
+                        value = NumberExpression(literalToken)
+
+                if self.current_token == TokenTypes.END_COMMAND:
+                    self.eat(TokenTypes.END_COMMAND)
+
+                return VarAssignSyntax(var_type, identifier, operator, value)
+
+    # endregion
+
+
 
     def parseGrammar(self) -> SyntaxTree:
-        result = self.parseConditionalExpression()
-        print(SyntaxEvaluator.evaluate(result))
+        result = self.parseSimpleStatement()
         pass
 
--- Parser/SyntaxTypes/Statement/Statement.py
+++ Parser/SyntaxTypes/Statement/Statement.py
@@ -1,4 +1,4 @@
 from Parser.SyntaxNode.SyntaxNode import SyntaxNode
 
-class Expression(SyntaxNode):
+class Statement(SyntaxNode):
     pass
