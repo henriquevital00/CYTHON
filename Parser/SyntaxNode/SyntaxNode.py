e50a06ce4a633f4d00294a57579579c9ccf8ddf1
felipepmoreno <felipepmoreno@gmail.com>
2021-11-17 21:39:37 -0300

    comentar do syntaxNode até o syntax visitor

--- Parser/SyntaxNode/SyntaxNode.py
+++ Parser/SyntaxNode/SyntaxNode.py
@@ -4,4 +4,9 @@ class SyntaxNode(ABC):
 
     @abstractmethod
     def getChildren(self) -> list:
+        """
+            Get the children of the tree
+
+            :return None
+        """
         pass
